a700cdd285d948fa5f839cb69389ac35444fa048
澁田謙太 <33084602+KentaShibuta@users.noreply.github.com>
2026-06-02 12:34:59 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MyCountRR",
+    products: [
+        .library(
+        			name: "MyCountRR",
+        			targets: ["MyCountRRTarget"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/KentaShibuta/MyOpenCV",
+            exact: "4.13.0"
+        ),
+        .package(
+            url: "https://github.com/microsoft/onnxruntime-swift-package-manager",
+            exact: "1.24.2"
+        )
+    ],
+    targets: [
+        .target(
+            name: "MyCountRRTarget",
+            dependencies: [
+                "MyCountRREngineBinary",
+                .product(name: "MyOpenCV", package: "MyOpenCV"),
+                .product(name: "OnnxRuntime", package: "onnxruntime-swift-package-manager")
+            ],
+            path: "Sources/MyCountRRTarget"
+        ),
+        .binaryTarget(
+            name: "MyCountRREngineBinary",
+            url: "https://github.com/KentaShibuta/MyCountRR/releases/download/1.0.0/engine.xcframework.zip", // .zip必須
+            checksum: "f93d19c5adada37480effd96adcc3296fa6205e9db487d40927ea26a9419c9df"
+        ),
+    ]
+)
